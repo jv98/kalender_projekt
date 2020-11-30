3691d4eb3da9927cbb4f7dc29a1708e4747c018b
jv98 <jacob@vahlen.se>
2020-11-30 14:43:14 +0100

    test

--- todlist.js
+++ todlist.js
@@ -8,7 +8,7 @@ function renderTodos() {
 
     for(const todo of todoList) {
         const listItem = document.createElement("li");
-        listItem.innerHTML = todo.date + ": " + todo.title;
+        listItem.innerHTML = todo.date + ": " + todo.title; 
         listItem.addEventListener('click', function(){
             todoList.splice(todoList.indexOf(todo),1)
             listItem.remove()
