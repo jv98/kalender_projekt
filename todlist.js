206f224b0cb4bf3329192b32d4fbd47690037146
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2020-12-01 15:35:14 +0100

    added trashcan

--- todlist.js
+++ todlist.js
@@ -8,7 +8,7 @@ function renderTodos() {
 
     for(const todo of todoList) {
         const listItem = document.createElement("li");
-        listItem.innerHTML = todo.date + ": " + todo.title; 
+        listItem.innerHTML = todo.date + ": " + todo.title + " " + '<i class="fas fa-trash-alt trash"></i>'; 
         listItem.addEventListener('click', function(){
             todoList.splice(todoList.indexOf(todo),1)
             listItem.remove()
